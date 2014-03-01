d4f422ffc996079c6a608d9eb291ee642ba4c533
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-02-28 22:12:08 -0500

    Add db schema with 1st Post migration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 class PostsController < ApplicationController
 
   def create
-    render text: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
   def new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 
 end
