82b08c921f318e18d91ec3317c3c822db80704ec
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-03-01 02:29:25 -0500

    Add delete Post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
